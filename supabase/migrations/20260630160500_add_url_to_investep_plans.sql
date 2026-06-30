be2e7a92d4aea3d8785199d8010715654eba417d
Hector Luis Lorenzo Zaldivar <lorenzozaldivar2023@gmail.com>
2026-06-30 17:10:26 +0200

    feat(plans): update plans seed migration to include product URLs

--- supabase/migrations/20260630160500_add_url_to_investep_plans.sql
+++ supabase/migrations/20260630160500_add_url_to_investep_plans.sql
@@ -1,1 +1,6 @@
 ALTER TABLE public.investep_plans ADD COLUMN url text;
+
+UPDATE public.investep_plans SET url = 'https://investepacademy.com/webinarbronce' WHERE slug = 'bronze';
+UPDATE public.investep_plans SET url = 'https://investepacademy.com/webinarsilver' WHERE slug = 'silver';
+UPDATE public.investep_plans SET url = 'https://investepacademy.com/webinaroro' WHERE slug = 'gold';
+UPDATE public.investep_plans SET url = 'https://investepacademy.com/webinarplatino' WHERE slug = 'platinum';
